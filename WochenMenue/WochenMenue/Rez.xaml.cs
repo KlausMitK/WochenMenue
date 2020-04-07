4909b2ad9778b5eccb6bdcc17838defd0b1b9a47
Klaus <Klaus@9a2fd316-128d-4348-9d11-99372ba87f9e>
2020-04-07 19:23:14 +0000

    R: RezMo -> Rez
    git-svn-id: svn://diskstation/Programmier%20Projekte@18 9a2fd316-128d-4348-9d11-99372ba87f9e

---
+++ WochenMenue/WochenMenue/Rez.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WochenMenue
+{
+    /// <summary>
+    /// Interaktionslogik für Window2.xaml
+    /// </summary>
+    public partial class Rez : Window
+    {
+        Wochenplan mParentWochenplan;
+        Tag mTag;
+
+        public Rez(Tag tag, Wochenplan wochenplan)
+        {
+            mTag = tag;
+            mParentWochenplan = wochenplan;
+            InitializeComponent();
+            lsV.ItemsSource = mTag.Rezept;
+        }
+
+        private void btn_0_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
