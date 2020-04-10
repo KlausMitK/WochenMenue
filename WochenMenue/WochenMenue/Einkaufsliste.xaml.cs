eda83ebc21782b7be58d587b303213bf660f762b
Dominik <Dominik@9a2fd316-128d-4348-9d11-99372ba87f9e>
2020-04-10 13:38:43 +0000

    N: Einkaufsliste
    git-svn-id: svn://diskstation/Programmier%20Projekte@30 9a2fd316-128d-4348-9d11-99372ba87f9e

---
+++ WochenMenue/WochenMenue/Einkaufsliste.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Windows.Documents.Serialization;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace WochenMenue
+{
+    /// <summary>
+    /// Interaktionslogik für Einkaufsliste.xaml
+    /// </summary>
+    public partial class Einkaufsliste : Window
+    {
+        public Einkaufsliste()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        
+
+        private void btn_Print_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
