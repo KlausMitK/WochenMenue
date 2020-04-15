9aa75bc40953e923bc86865311379ed2ede2075d
Klaus <Klaus@9a2fd316-128d-4348-9d11-99372ba87f9e>
2020-04-15 20:18:11 +0000

    N: RezeptPool.cs
    git-svn-id: svn://diskstation/Programmier%20Projekte@41 9a2fd316-128d-4348-9d11-99372ba87f9e

---
+++ WochenMenue/WochenMenue/RezeptPool.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+namespace WochenMenue
+{
+    [Serializable]
+    class RezeptPool
+    {
+
+        private Dictionary<string, List<Zutat>> mRezeptMap;
+
+        //Constructor
+        public RezeptPool()
+        {
+            mRezeptMap = new Dictionary<string, List<Zutat>>();
+        }
+
+        public Dictionary<string, List<Zutat>> RezeptMap
+        {
+            get{ return mRezeptMap;}
+            set { mRezeptMap = value;}
+        }
+
+        public void AddGericht(string Gericht, List<Zutat> ZutatenListe)
+        {
+            mRezeptMap.Add(Gericht, ZutatenListe);
+        }
+        
+        public List<Zutat> FindeZutatenFuer(string Gericht)
+        {
+            return mRezeptMap[Gericht];
+        }
+    }
+}
