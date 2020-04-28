3dd5a420f2aeae29bc9e6728768427cb146e1dd0
Dominik <Dominik@9a2fd316-128d-4348-9d11-99372ba87f9e>
2020-04-28 19:05:29 +0000

    R: ZutatTest Test für toString Methode
    git-svn-id: svn://diskstation/Programmier%20Projekte@96 9a2fd316-128d-4348-9d11-99372ba87f9e

---
+++ WochenMenue/UnitTests/ZutatTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WochenMenue;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    class ZutatTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void zutatTest() 
+        {
+            Zutat zutat = new Zutat();
+
+            zutat.Name = "milch";
+            zutat.Menge = 1;
+            zutat.Einheit = "Liter";
+
+            Assert.AreEqual("milch: 1 Liter", zutat.ToString());
+        }
+        
+
+        
+    }
+}
