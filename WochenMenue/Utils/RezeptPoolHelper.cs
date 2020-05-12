649f8ba72479dd45e03f2a2969346eac694f05c9
Klaus <Klaus@9a2fd316-128d-4348-9d11-99372ba87f9e>
2020-05-12 19:22:10 +0000

    R: RezeptPoolHelper in Utils
    git-svn-id: svn://diskstation/Programmier%20Projekte@122 9a2fd316-128d-4348-9d11-99372ba87f9e

---
+++ WochenMenue/Utils/RezeptPoolHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using BusinessLogic;
+
+
+namespace Utils
+{
+    public class RezeptPoolHelper
+    {
+        public RezeptPoolHelper()
+        {
+
+        }
+
+        public void GerichtInPool(Tag tag)
+        {
+            string GerichtName = tag.Gericht;
+
+            Gericht gericht = new Gericht(GerichtName);
+            foreach (Zutat zutat in tag.Zutaten)
+            {
+                gericht.Zutaten.Add(zutat);
+            }
+
+            Logging.Instance().Info("RezeptPool: " + PropValues.Instance().PoolPath + " wird geladen...");
+
+            RezeptPool rezPool = new RezeptPool();
+            XmlSerializer serializer = new XmlSerializer(typeof(RezeptPool));
+            FileStream fileStream = new FileStream(PropValues.Instance().PoolPath, FileMode.Open);
+            rezPool = (RezeptPool)serializer.Deserialize(fileStream);
+            fileStream.Close();
+
+           Logging.Instance().Info("RezeptPool: " + PropValues.Instance().PoolPath + " ist geladen");
+
+            if (rezPool.FindeGericht(GerichtName) == null)
+            {
+                rezPool.AddGericht(gericht);
+            }
+            else
+            {
+                Logging.Instance().Warning("Das Gericht " + GerichtName + " ist bereits im RezeptPool vorhanden");
+                return; // Das folgende muss in dem Fall nicht mehr gemacht werden.
+            }
+
+
+            Logging.Instance().Info("RezeptPool: " + PropValues.Instance().PoolPath + " wird aktualisiert ...");
+
+            serializer = new XmlSerializer(typeof(RezeptPool));
+            FileStream filestream = new FileStream(PropValues.Instance().PoolPath, FileMode.Create);
+            serializer.Serialize(filestream, rezPool);
+            filestream.Close();
+
+            Logging.Instance().Info("RezeptPool: " + PropValues.Instance().PoolPath + " ist aktualisiert");
+        }
+    }
+}
--- WochenMenue/WochenMenue/MainWindow.xaml.cs
+++ WochenMenue/WochenMenue/MainWindow.xaml.cs
@@ -222,43 +222,8 @@ namespace WochenMenue
 
         private void GerichtInPool(Tag tag)
         {
-            string GerichtName = tag.Gericht;
-
-            Gericht gericht = new Gericht(GerichtName);
-            foreach (Zutat zutat in tag.Zutaten)
-            {
-                gericht.Zutaten.Add(zutat);
-            }
-
-            MainWindow.gLog.Info("RezeptPool: " + PropValues.Instance().PoolPath + " wird geladen...");
-
-            RezeptPool rezPool = new RezeptPool();
-            XmlSerializer serializer = new XmlSerializer(typeof(RezeptPool));
-            FileStream fileStream = new FileStream(PropValues.Instance().PoolPath, FileMode.Open);
-            rezPool = (RezeptPool)serializer.Deserialize(fileStream);
-            fileStream.Close();
-
-            MainWindow.gLog.Info("RezeptPool: " + PropValues.Instance().PoolPath + " ist geladen");
-
-            if (rezPool.FindeGericht(GerichtName) == null)
-            {
-                rezPool.AddGericht(gericht);
-            }
-            else
-            {
-                MainWindow.gLog.Warning("Das Gericht " + GerichtName + " ist bereits im RezeptPool vorhanden");
-                return; // Das folgende muss in dem Fall nicht mehr gemacht werden.
-            }
-
-
-            MainWindow.gLog.Info("RezeptPool: " + PropValues.Instance().PoolPath + " wird aktualisiert ...");
-
-            serializer = new XmlSerializer(typeof(RezeptPool));
-            FileStream filestream = new FileStream(PropValues.Instance().PoolPath, FileMode.Create);
-            serializer.Serialize(filestream, rezPool);
-            filestream.Close();
-
-            MainWindow.gLog.Info("RezeptPool: " + PropValues.Instance().PoolPath + " ist aktualisiert");
+            RezeptPoolHelper rezPoolHelper = new Utils.RezeptPoolHelper();
+            rezPoolHelper.GerichtInPool(tag);
         }
 
         // Stackpanel --> Hizufügen
