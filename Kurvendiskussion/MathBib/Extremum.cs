e1260ab954e8aa8ca4e60494002330d5d4a6f455
Klaus <Klaus@9a2fd316-128d-4348-9d11-99372ba87f9e>
2021-03-31 20:19:29 +0000

    N: Extremum Added
    git-svn-id: svn://diskstation/Programmier%20Projekte@148 9a2fd316-128d-4348-9d11-99372ba87f9e

---
+++ Kurvendiskussion/MathBib/Extremum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathBib
+{
+    class Extremum : Point
+    {
+        public enum ExtremType
+        {
+            Min,
+            Max
+        }
+
+        private ExtremType extremType;
+
+        public ExtremType MinMaxType
+        {
+            get {return extremType; }
+            set {extremType = value;}
+        }
+
+    }
+}
