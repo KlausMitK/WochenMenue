52a0fb6612d4fb37e6f34997006c4afe29c54dc7
Klaus <Klaus@9a2fd316-128d-4348-9d11-99372ba87f9e>
2021-04-08 11:47:12 +0000

    N: NewtonSolver implementiert
    git-svn-id: svn://diskstation/Programmier%20Projekte@179 9a2fd316-128d-4348-9d11-99372ba87f9e

---
+++ Kurvendiskussion/MathBib/NewtonSolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace MathBib
+{
+    class NewtonSolver : ISolver
+    {
+        public IMathFunc TheFunction
+        {
+            get;set;
+        }
+
+        public double StartPoint
+        {
+            get;set;
+        }
+
+        public double Accuracy
+        {
+            get;set;
+        }
+
+        public NewtonSolver(IMathFunc function, double startpoint, double accuracy)
+        {
+            TheFunction = function;
+            StartPoint = startpoint;
+            Accuracy = accuracy;
+        }
+
+        public ObservableCollection<double> Solve()
+        {
+            ObservableCollection<double> retCol = new ObservableCollection<double>();
+
+            double xNeu = StartPoint;
+            double xAlt = StartPoint;
+
+            IMathFunc Derivative = TheFunction.Derivative();
+
+            //TODO: Ableistungsfunktion in einer Variable speichern
+
+            // TODO: Solange bis der Quotient der Betra von (Xn+1/Xn) - 1> Accuray ist
+            // Neues Xn+1 berechnen nach Formel;
+            do
+            {
+                xAlt = xNeu;
+                xNeu = xAlt - (TheFunction.Calculate(xAlt) / Derivative.Calculate(xAlt));
+
+            } while (Math.Abs(xNeu / xAlt) - 1 <= Accuracy);
+
+            retCol.Add(xNeu);
+            return retCol;
+        }
+    }
+}
