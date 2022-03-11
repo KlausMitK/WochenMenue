5341920952a8e086d01684d6df59447a416bcd86
Klaus <Klaus@9a2fd316-128d-4348-9d11-99372ba87f9e>
2022-03-11 16:19:37 +0000

    R: Einfügen und Editieren von Grichten im Pool-Editor.
    git-svn-id: svn://diskstation/Programmier%20Projekte@186 9a2fd316-128d-4348-9d11-99372ba87f9e

--- WochenMenue/BusinessLogic/Gericht.cs
+++ WochenMenue/BusinessLogic/Gericht.cs
@@ -1,24 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace BusinessLogic
 {
     [Serializable]
-    public class Gericht
+    public class Gericht : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public List<Zutat> Zutaten { get; set; }
+        private string mName;
+        public string Name 
+        {   get
+            {
+                return mName;
+            }
+            set
+            {
+                mName = value;
+                NotifyPropertyChanged("Name"); 
+            }
+        }
+
+        private ObservableCollection<Zutat> mZutaten;
+        public ObservableCollection<Zutat> Zutaten 
+        { get
+            {
+                return mZutaten;
+            }
+            set
+            {
+                mZutaten = value;
+                NotifyPropertyChanged("Zutaten");
+            }
+        }
+
+        private int mPropChangedCount;
+
+        public int PropChangeCount()
+        {
+            return mPropChangedCount;
+        }
 
         public Gericht (string name)
         {
             Name = name;
-            Zutaten = new List<Zutat>();
+            Zutaten = new ObservableCollection<Zutat>();
         }
 
         public Gericht ()
         {
             Name = "";
-            Zutaten = new List<Zutat>();
+            Zutaten = new ObservableCollection<Zutat>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+            mPropChangedCount++;
         }
     }
 }
--- WochenMenue/WochenMenue/PoolEditor.xaml.cs
+++ WochenMenue/WochenMenue/PoolEditor.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BusinessLogic;
+using Utils;
 
 namespace WochenMenue
 {
@@ -19,9 +21,54 @@ namespace WochenMenue
     /// </summary>
     public partial class PoolEditor : Window
     {
+        RezeptPool rezPool { get; set; }
         public PoolEditor()
         {
             InitializeComponent();
+
+            //Lade Pool (Deserialisieren)
+            MainWindow.gLog.Info("RezeptPool: " + PropValues.Instance().PoolPath + " wird geladen...");
+            rezPool = RezeptPool.Load(PropValues.Instance().PoolPath);
+            MainWindow.gLog.Info("RezeptPool: " + PropValues.Instance().PoolPath + " ist geladen");
+
+            //Binding
+            GerichtList.ItemsSource = rezPool.Gerichte;
         }
+
+        private void GerichtList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           try
+            { 
+                Gericht gericht = (Gericht)GerichtList.SelectedItem;
+                if (gericht != null)
+                {
+                    if (gericht.Zutaten.Count() > 0)
+                    {
+                        ZutatenList.ItemsSource = gericht.Zutaten;
+                    }
+                
+                }
+            }
+            catch (System.InvalidCastException)
+            {
+                Gericht ger = new Gericht("XXX");
+                Zutat zutat = new Zutat("xxx", 0, "yyy");
+                ger.Zutaten.Add(zutat);
+                rezPool.AddGericht(ger);
+
+                ZutatenList.ItemsSource = ger.Zutaten;
+            }
+        }
+
+        private void NeuGer_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Gericht ger = new Gericht("XXX");
+            Zutat zutat = new Zutat("xxx", 0, "yyy");
+            ger.Zutaten.Add(zutat);
+            rezPool.AddGericht(ger);
+            
+            ZutatenList.ItemsSource = ger.Zutaten;
+        }
+        
     }
 }
